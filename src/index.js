85b734f56ed79aac3a98a71297ace15cf4631d81
JoshuaToback <joshuatoback@gmail.com>
2023-05-23 14:30:33 -0400

    Edited routing again

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='joshuatobackportfolio'>
+  <BrowserRouter basename='jt-portfolio'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
